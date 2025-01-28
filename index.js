a25b92ccef854eac82af9843fcd0863708f6e749
sweksha2759 <sweksha2759@csmu.ac.in>
2025-01-28 11:24:16 +0000

    add all the fixes

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-import { readCSV, writeCSV } from "./lib/io.js";
-
 // Read CLI Arguments
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 const argv = yargs(hideBin(process.argv)).argv;
+
+import { readCSV, writeCSV } from "./lib/io.js";
+import service from "./lib/service.js";
+
+// This is responsible to start our program
+function main() {
+  const csvData = readCSV(argv.input);
+  const [clean, errors] = service(csvData.body);
+
+  // Create the clean file
+  writeCSV(argv.clean, clean);
+
+  // Generate report file
+  writeCSV(argv.errors, errors);
+}
+main();
